1e7125eeb64f15e0cdc7f8e8c0742dfff307f6cc
Kanish <111358462+kanish13@users.noreply.github.com>
2024-08-06 15:07:20 +0530

    Update football_app.py

--- football_app.py
+++ football_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import base64
 
-
 st.title('NFL Football Stats')
 
 st.markdown("""
@@ -19,33 +18,40 @@ selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2020))))
 
 @st.cache_data
 def load_data(year):
-    url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
-    html = pd.read_html(url, header = 1)
-    df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-    raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis=1)
-    return playerstats
+    try:
+        url = f"https://www.pro-football-reference.com/years/{year}/rushing.htm"
+        html = pd.read_html(url, header=1)
+        df = html[0]
+        raw = df.drop(df[df.Age == 'Age'].index)  # Deletes repeating headers in content
+        raw = raw.fillna(0)
+        playerstats = raw.drop(['Rk'], axis=1)
+        return playerstats
+    except Exception as e:
+        st.error(f"Error loading data: {e}")
+        return pd.DataFrame()  # Return an empty DataFrame in case of an error
+
 playerstats = load_data(selected_year)
 
-
 # Sidebar - Team selection
-sorted_unique_team = sorted(playerstats.Tm.unique())
-selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
-
+if not playerstats.empty:
+    sorted_unique_team = sorted(playerstats.Tm.unique())
+    selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+else:
+    selected_team = []
 
 # Sidebar - Position selection
-unique_pos = ['RB','QB','WR','FB','TE']
+unique_pos = ['RB', 'QB', 'WR', 'FB', 'TE']
 selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
-
 
 # Filtering data
 df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
 st.header('Display Player Stats of Selected Team(s)')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-st.dataframe(df_selected_team)
-
+if not df_selected_team.empty:
+    st.write(f'Data Dimension: {df_selected_team.shape[0]} rows and {df_selected_team.shape[1]} columns.')
+    st.dataframe(df_selected_team)
+else:
+    st.write('No data available for the selected team(s) and position(s).')
 
 # Download NBA player stats data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
@@ -55,5 +61,5 @@ def filedownload(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
     return href
 
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
-
+if not df_selected_team.empty:
+    st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
